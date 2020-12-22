275ab19c85afba3f80d2f43d1ca6b8c7dd6e4f88
ZippyMagician <zippymagician1@gmail.com>
2020-12-22 11:49:48 -0500

    Try to fix failed build

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ for (file of files) {
     if (file.endsWith('.arsm')) {
         map[file.slice(0, file.length - 5)][0] = 'test_cases/' + file;
     } else if (file.endsWith('.in')) {
-        map[file.slice(0, file.length - 3)][1] = 'test_cases/' + file;
+        map[file.slice(0, file.length - 3)][1] = fs.readFileSync('test_cases/' + file).toString();
     } else if (file.endsWith('.out')) {
         map[file.slice(0, file.length - 4)][2] = fs.readFileSync('test_cases/' + file).toString();
     }
@@ -18,13 +18,15 @@ for (file of files) {
 
 for (let c in map) {
     c = map[c];
-    exec(`cargo -q run ${c[0]} --stdin ${c[1]}`,  (_, stdout) => {
+    exec(`cargo -q run ${c[0]} "${c[1]}"`,  (err, stdout, stderr) => {
         if (stdout == c[2]) {
             console.log("Case " + c[0] + ": \x1b[32m Passed\x1b[0m");
         } else {
             console.log("Case " + c[0] + ": \x1b[31m Failed\x1b[0m");
             console.log("  Expected: " + c[2]);
             console.log("  Got:      " + stdout);
+
+            process.exit(1);
         }
     });
 }
