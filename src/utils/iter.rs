92d21a876b4c7daf1a32aed2ec6318b2733b30bc
ZippyMagician <zippymagician1@gmail.com>
2021-01-08 13:47:42 -0500

    Greatly improve speed of BufIter (2.3ms -> 700ns / 100k)

--- src/utils/iter.rs
+++ src/utils/iter.rs
@@ -1,4 +1,4 @@
-use std::ptr;
+use std::ptr::{self, NonNull};
 
 // A custom iterator implementation that allows what `.iter()` does not for stdin
 // Namely, there were issues to do with the lifetime of the string contents of a read file
@@ -6,21 +6,15 @@ use std::ptr;
 // It is also rather fast, making use of std::ptr
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub struct BufIter<T> {
-    ptr: *mut T,
-    end: *const T,
+    ptr: NonNull<T>,
+    c: usize,
 }
 
 impl<T> BufIter<T> {
-    pub fn new(body: &mut [T]) -> Self {
-        assert_ne!(std::mem::size_of::<T>(), 0);
-        assert!(body.len() <= std::isize::MAX as usize);
-        // Safety: Previous conditions ensure this will work and not result in UB
-        unsafe {
-            let ptr = body.as_mut_ptr();
-            Self {
-                ptr,
-                end: ptr.add(body.len()),
-            }
+    pub fn new(slice: &mut [T]) -> Self {
+        Self {
+            ptr: NonNull::new(slice.as_mut_ptr()).unwrap(),
+            c: slice.len(),
         }
     }
 }
@@ -28,17 +22,14 @@ impl<T> BufIter<T> {
 impl<T> Iterator for BufIter<T> {
     type Item = T;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        // Safety: meets all requirements of pointer::offset_from, therefore safe
-        if unsafe { self.ptr.offset_from(self.end) } == 0 {
+    fn next(&mut self) -> Option<T> {
+        if self.c == 0 {
             None
         } else {
-            let old = self.ptr;
-            // Safety: Wrapping code ensures this cannot overflow or read a null pointer
-            unsafe {
-                self.ptr = self.ptr.offset(1);
-                Some(ptr::read(old))
-            }
+            self.c -= 1;
+            let out = unsafe { ptr::read(self.ptr.as_ptr()) };
+            self.ptr = unsafe { NonNull::new_unchecked(self.ptr.as_ptr().offset(1)) };
+            Some(out)
         }
     }
 }
